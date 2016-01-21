b917e57820bf7997432322fd2edcdd8f9c99a23f
barczaG <gergo@barcza.hu>
2016-01-21 16:58:00 +0100

    Changes

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
-var express = require("express");
-var app = express();
-var port = 8888;
+var express = require('express')
+var app = express()
+var port = 8888
 
-app.set('views', __dirname + '/tpl');
-app.set('view engine', "jade");
-app.engine('jade', require('jade').__express);
-app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/tpl')
+app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/public'))
 
-app.get("/", function(req, res){
-	res.render("page");
-});
+app.get('/', function (req, res) {
 
-var io = require('socket.io').listen(app.listen(port));
+  var message = req.query.message
+  res.render('page', {
+    welcomeMessage: message,
+    dolgok: ['valami1', 'foo', 'bar']
+  })
+})
+
+var io = require('socket.io').listen(app.listen(port))
 
 io.sockets.on('connection', function (socket) {
-	socket.emit('message', { message: 'welcome to the chat' });
-	socket.on('send', function (data) {
-		io.sockets.emit('message', data);
-	});
-});
-console.log("Listening on port " + port);
+  socket.emit('message', { message: 'welcome to the chat' })
+  socket.on('send', function (data) {
+    io.sockets.emit('message', data)
+  })
+
+  setTimeout(function () {
+    socket.emit('alert')
+  }, 10 * 1000)
+})
+console.log('Listening on port ' + port)
--- public/chat.js
+++ public/chat.js
@@ -1,41 +1,47 @@
-window.onload = function() {
+/* global io, alert, $ */
 
-	var messages = [];
-	var socket = io.connect('http://'+location.host);
-	var field = document.getElementById("field");
-	var sendButton = document.getElementById("send");
-	var content = document.getElementById("content");
-	var name = document.getElementById("name");
+var sendMessage
+window.onload = function () {
+  var messages = []
+  var socket = io.connect('http://' + window.location.host)
+  var field = document.getElementById('field')
+  var sendButton = document.getElementById('send')
+  var content = document.getElementById('content')
+  var name = document.getElementById('name')
 
-	socket.on('message', function (data) {
-		if(data.message) {
-			messages.push(data);
-			var html = '';
-			for(var i=0; i<messages.length; i++) {
-				html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
-				html += messages[i].message + '<br />';
-			}
-			content.innerHTML = html;
-		} else {
-			console.log("There is a problem:", data);
-		}
-	});
+  socket.on('message', function (data) {
+    if (data.message) {
+      messages.push(data)
+      var html = ''
+      for (var i = 0; i < messages.length; i++) {
+        html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>'
+        html += messages[i].message + '<br />'
+      }
+      content.innerHTML = html
+    } else {
+      console.log('There is a problem:', data)
+    }
+  })
 
-	sendButton.onclick = sendMessage = function() {
-		if(name.value == "") {
-			alert("Please type your name!");
-		} else {
-			var text = field.value;
-			socket.emit('send', { message: text, username: name.value });
-			field.value = "";
-		}
-	};
+  socket.on('alert', function (data) {
+    alert('Már legalább 10 perce vagy az oldalon!')
+  })
 
+  sendMessage = sendButton.onclick = function () {
+    if (name.value === '') {
+      alert('Please type your name!')
+    } else {
+      var text = field.value
+      socket.emit('send', { message: text, username: name.value })
+      field.value = ''
+    }
+  }
 }
-$(document).ready(function() {
-	$("#field").keyup(function(e) {
-		if(e.keyCode == 13) {
-			sendMessage();
-		}
-	});
-});
+$(document).ready(function () {
+  $('#field').keyup(function (e) {
+    if (e.keyCode === 13) {
+      sendMessage()
+    }
+  })
+})
+
